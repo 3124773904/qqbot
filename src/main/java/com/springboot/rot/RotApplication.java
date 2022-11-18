bdfcfb509c501e807d8808b548a8361251cbf662
xw <3124773904@qq.com>
2022-11-18 14:36:00 +0800

    创建

--- src/main/java/com/springboot/rot/RotApplication.java
+++ src/main/java/com/springboot/rot/RotApplication.java
@@ -2,7 +2,9 @@ package com.springboot.rot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableSimbot
 @SpringBootApplication
 public class RotApplication {
 
